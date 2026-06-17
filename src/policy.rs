0b0039f2708f48f0ef2347c0e7a2eb2984572664
Samuel ORTION <samuel.ortion@genoscope.cns.fr>
2026-06-17 10:52:01 +0200

    comment unused code

--- src/policy.rs
+++ src/policy.rs
@@ -1,6 +1,12 @@
 #[derive(Debug, Clone)]
 pub struct Policy;
 
+impl Default for Policy {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl Policy {
     pub fn new() -> Self {
         Policy {}
